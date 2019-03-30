bd90e5707c74fbc0a5089fbd894e3061c2a5f3bd
thundermindtech <thundermindtech@gmail.com>
2019-03-30 10:50:34 +0500

    Added error message

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -15,35 +15,29 @@ class Register extends Component {
       errors: {}
     };
   }
-  componentDidMount() {
-    // If logged in and user navigates to Register page, should redirect them to dashboard
-    if (this.props.auth.isAuthenticated) {
-      this.props.history.push("/dashboard");
+componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors
+      });
     }
-  } 
-  // componentWillReceiveProps(nextProps) {
-  //   if (nextProps.errors) {
-  //     this.setState({
-  //       errors: nextProps.errors
-  //     });
-  //   }
-  // }
-  onChange = e => {
+  }
+onChange = e => {
     this.setState({ [e.target.id]: e.target.value });
   };
-  onSubmit = e => {
+onSubmit = e => {
     e.preventDefault();
-    const newUser = {
+const newUser = {
       name: this.state.name,
       email: this.state.email,
       password: this.state.password,
       password2: this.state.password2
     };
-    this.props.registerUser(newUser, this.props.history); 
+this.props.registerUser(newUser, this.props.history); 
   };
-  render() {
+render() {
     const { errors } = this.state;
-      return (
+return (
       <div className="container">
         <div className="row">
           <div className="col s8 offset-s2">
@@ -64,45 +58,57 @@ class Register extends Component {
                 <input
                   onChange={this.onChange}
                   value={this.state.name}
+                  error={errors.name}
                   id="name"
                   type="text"
-                 
+                  className={classnames("", {
+                    invalid: errors.name
+                  })}
                 />
                 <label htmlFor="name">Name</label>
-                <span className="red-text"></span>
+                <span className="red-text">{errors.name}</span>
               </div>
               <div className="input-field col s12">
                 <input
                   onChange={this.onChange}
                   value={this.state.email}
+                  error={errors.email}
                   id="email"
                   type="email"
-                  
+                  className={classnames("", {
+                    invalid: errors.email
+                  })}
                 />
                 <label htmlFor="email">Email</label>
-                <span className="red-text"></span>
+                <span className="red-text">{errors.email}</span>
               </div>
               <div className="input-field col s12">
                 <input
                   onChange={this.onChange}
                   value={this.state.password}
+                  error={errors.password}
                   id="password"
                   type="password"
-                  
+                  className={classnames("", {
+                    invalid: errors.password
+                  })}
                 />
                 <label htmlFor="password">Password</label>
-                <span className="red-text"></span>
+                <span className="red-text">{errors.password}</span>
               </div>
               <div className="input-field col s12">
                 <input
                   onChange={this.onChange}
                   value={this.state.password2}
+                  error={errors.password2}
                   id="password2"
                   type="password"
-                  
+                  className={classnames("", {
+                    invalid: errors.password2
+                  })}
                 />
                 <label htmlFor="password2">Confirm Password</label>
-                <span className="red-text"></span>
+                <span className="red-text">{errors.password2}</span>
               </div>
               <div className="col s12" style={{ paddingLeft: "11.250px" }}>
                 <button
@@ -128,9 +134,11 @@ class Register extends Component {
 Register.propTypes = {
   registerUser: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
 };
 const mapStateToProps = state => ({
   auth: state.auth,
+  errors: state.errors
 });
 export default connect(
   mapStateToProps,
